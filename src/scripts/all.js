db4b8953a0b8000c2936ae9531502227ea70f833
yamasy1549 <sanae@yamasy.info>
2016-01-26 22:24:07 +0900

    Add drawer menu for mobile

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -1,10 +1,38 @@
-$(function(){
-    $('a[href^=#]').click(function() {
-        var speed = 400;
-        var href= $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top - 80;
-        $('body,html').animate({scrollTop:position}, speed, 'swing');
-        return false;
+$(document).ready(function() {
+    open = new Boolean(false);
+
+    $(function(){
+        $('a[href^=#]').click(function() {
+            var speed = 400;
+            var href= $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top - 80;
+            $('body,html').animate({scrollTop:position}, speed, 'swing');
+            $('.menu-btn').click();
+        });
+    });
+
+    $('.menu-btn').click(function() {
+        if(open == true) {
+            $('.menu').animate({
+                height: '60px'
+            }, 500);
+            $('.nav-item').animate({
+                display: 'none'
+            },{
+                'duration': 500,
+                'complete': function() {
+                    $('.nav-item').css('display', 'none');
+                    $('.nav-item:first-child').css('display', 'block');
+                }
+            });
+            open = false;
+        } else {
+            $('.menu').animate({
+                height: '370px'
+            }, 500);
+            $('.nav-item').css('display', 'block');
+            open = true;
+        };
     });
 });
