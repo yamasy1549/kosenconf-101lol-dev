8d8fd52251239cf0011794b68f7dcb3c9f5b8669
yamasy1549 <sanae@yamasy.info>
2016-02-07 14:56:13 +0900

    global-navの挙動を修正

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -1,26 +1,12 @@
 $(document).ready(function() {
     open = new Boolean(false);
 
-    $(function(){
-        $('a[href^=#]').click(function() {
-            var speed = 400;
-            var href= $(this).attr("href");
-            var target = $(href == "#" || href == "" ? 'html' : href);
-            var position = target.offset().top - 20;
-            $('body,html').animate({scrollTop:position}, speed, 'swing');
-            $('.menu').animate({
-                height: '60px'
-            }, 500);
-            $('.nav-item').animate({
-                display: 'none'
-            },{
-                'duration': 500,
-                'complete': function() {
-                    $('.nav-item').css('display', 'none');
-                    $('.nav-item:first-child').css('display', 'block');
-                }
-            });
-        });
+    $('.wrapper a[href^=#]').click(function() {
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - 20;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
     });
 
     if(location.pathname != "/") {
